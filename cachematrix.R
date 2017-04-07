0d0dfe20377dbe1ea89a5781e454a2e9b577cf0c
mabroome <mark.a.broome@gmail.com>
2017-04-07 15:21:36 -0400

    Correct Version, Firs version was shell

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This is a set of functions get and set the inverse Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv_mat) m <<- inv_mat
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function will first check to see if the inverse
+#  matrix is saved in cache, If yes return matrix, if no
+#  calculate the inverse and save to cache. Function
+#  makeCacheMatrix gets and sets the inverse matrix in cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached inverse matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
